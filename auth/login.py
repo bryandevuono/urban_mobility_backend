d604938835ebfa8611da9721053faaa40622a4eb
bryandevuono <bryandevuono@gmail.com>
2025-05-22 11:07:45 +0200

    22-5

---
+++ auth/login.py
@@ -0,0 +1,8 @@
+def authenticate_user(username, password):
+    #validate input (required, length format)
+    #hard-coded
+    if username == "super_admin" and "Admin123?":
+        return True
+    #check if username exists
+
+    #check password
---
+++ interface/login.py
@@ -0,0 +1,17 @@
+clear = lambda: os.system('clear')
+def welcome_screen():
+    print("Welcome to the urban mobility system\n"
+           "Choose an option:\n"
+           "\n Login (enter L)\n"
+           "Register (enter R)")
+    login_option = input()
+
+    if (login_option.lower() == "l" and len(login_option) < 2):
+        clear()
+        login_screen()
+
+def login_screen():
+    while True:
+        username = input("Enter your username\n")
+        password = input("Enter your password\n")
+    login = Auth.login.authenticate_user(username, password)
