5df85a6d774b69aca5e4ec1e58a46cf29b279fdf
bryandevuono <114662794+bryandevuono@users.noreply.github.com>
2025-05-23 08:32:29 +0000

    soad

--- auth/login.py
+++ auth/login.py
@@ -2,7 +2,7 @@ import sqlite3
 def authenticate_user(username, password):
     #validate input (required, length format)
     #hard-coded
-    if username == "super_admin" and "Admin123?":
+    if username == "super_admin" and password == "Admin123?":
         return True
     #check if username exists
 
--- interface/login.py
+++ interface/login.py
@@ -1,8 +1,16 @@
+import os
+import sys
+
+sys.path.insert(0, '../auth')
+
+from login import authenticate_user 
+
 clear = lambda: os.system('clear')
+
 def welcome_screen():
     print("Welcome to the urban mobility system\n"
            "Choose an option:\n"
-           "\n Login (enter L)\n"
+           "\nLogin (enter L)\n"
            "Register (enter R)")
     login_option = input()
 
@@ -16,11 +24,17 @@ def login_screen():
     while True:
         username = input("Enter your username\n")
         password = input("Enter your password\n")
-        login = Auth.login.authenticate_user(username, password)
-
+        login_attempt = authenticate_user(username, password)
+        if login_attempt:
+            print("logged in")
+            break
+        else:
+            continue
+            
 def signup_screen():
     while True:
         username = input("Enter your username\n")
         password = input("Enter your password\n")
         firstname = input("Enter your password\n")
         lastname = input("Enter your password\n")
+welcome_screen()
