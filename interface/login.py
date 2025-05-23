7e5ec99c0eb22a502cb161a19129c337393e4f84
bryandevuono <bryandevuono@gmail.com>
2025-05-23 09:10:41 +0200

    23-5

--- interface/login.py
+++ interface/login.py
@@ -9,9 +9,18 @@ def welcome_screen():
     if (login_option.lower() == "l" and len(login_option) < 2):
         clear()
         login_screen()
+    else:
+        print("Wrong key, please try again")
 
 def login_screen():
     while True:
         username = input("Enter your username\n")
         password = input("Enter your password\n")
-    login = Auth.login.authenticate_user(username, password)
+        login = Auth.login.authenticate_user(username, password)
+
+def signup_screen():
+    while True:
+        username = input("Enter your username\n")
+        password = input("Enter your password\n")
+        firstname = input("Enter your password\n")
+        lastname = input("Enter your password\n")
